3b09a910120dbd3217ae3a2b1b3550d778744eb2
Marcelo V Dias <marcelovdias@gmail.com>
2026-02-06 15:32:04 -0400

    Fix: Add .tsx extension to App import and appearance CSS property

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
